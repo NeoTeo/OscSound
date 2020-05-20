db3b874b6fc18ef6c2651cdbd976f0e731edb3a1
Teo Sartori <m@sartori.dk>
2020-05-20 22:23:54 +0200

    Initial Commit

---
+++ OscSound/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  OscSound
+//
+//  Created by teo on 20/05/2020.
+//  Copyright © 2020 teos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
